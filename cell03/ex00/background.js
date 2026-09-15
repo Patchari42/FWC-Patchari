5cc7508e517512fa17a613d2053961b4ecde1db8
Patchari42 <67010687@kmitl.com>
2026-09-16 00:38:21 +0700

    update files

--- cell03/ex00/background.js
+++ cell03/ex00/background.js
@@ -1,18 +1,13 @@
 const button = document.getElementById('change-btn');
 
 
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-
 button.addEventListener('click', function() {
-    const randomColor = getRandomColor();
     
+    const r = Math.floor(Math.random() * 256); 
+    const g = Math.floor(Math.random() * 256); 
+    const b = Math.floor(Math.random() * 256); 
+
+    const randomColor = `rgb(${r}, ${g}, ${b})`;
+
     document.body.style.backgroundColor = randomColor;
 });
