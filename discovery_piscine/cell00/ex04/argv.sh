557dfe1f4c53b669fc8973000ec3efd969e6fef2
Patchari42 <67010687@kmitl.com>
2026-09-16 16:55:52 +0700

    update files

--- discovery_piscine/cell00/ex04/argv.sh
+++ discovery_piscine/cell00/ex04/argv.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
-
-args=("$@")
-
-if [ ${#args[@]} -eq 0 ]; then
+if [ $# -eq 0 ]; then
     echo "No arguments supplied"
 else
-    for ((i=0; i<3 && i<${#args[@]}; i++)); do
-        echo "${args[i]}"
-    done
-fi             
+    if [ -n "$1" ]; then
+        echo "$1"
+    fi
+    if [ -n "$2" ]; then
+        echo "$2"
+    fi
+    if [ -n "$3" ]; then
+        echo "$3"
+    fi
+fi
